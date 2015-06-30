f9afe11b03df98836c2e0269296dbf8344beeee6
JackRFuller <jackrfuller@outlook.com>
2015-06-30 21:33:20 +0100

    Jump/Input Implemented
    Input mask & jumping behaviour implemented

---
+++ Machination_Unity/Assets/Scripts/Behaviours/DetectInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectInput : MonoBehaviour {
+
+    private PlayerBehaviour PB_Script;
+
+	// Use this for initialization
+    void Start()
+    {
+        PB_Script = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+	
+	}
+    
+
+    void OnTouchUp()
+    {
+        PB_Script.Jump();
+    }
+}
---
+++ Machination_Unity/Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    [Header("Jumping Variables")]
+    [SerializeField] private float JumpStrength;
+    [SerializeField] private float Gravity;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private CharacterController PC_Controller;
+    
+    
+
+	// Use this for initialization
+	void Start () {
+
+       
+        PC_Controller = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveDirection.y -= Gravity * Time.deltaTime;
+        PC_Controller.Move(moveDirection * Time.deltaTime);
+	}
+
+    public void Jump()
+    {
+        moveDirection.y = JumpStrength;
+        
+
+    }
+}
